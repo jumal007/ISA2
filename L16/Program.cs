d6a6c59496850a4867ece8a79ce32be9694aa31d
Ilja Freiberg <ilfrei@ttu.ee>
2019-02-04 14:27:44 +0200

    number 16 save

---
+++ L16/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace L16
+{
+    class Program
+    {
+        private static string k = "";
+
+        static void Main(string[] args)
+        {
+            string j = "";
+
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(i);
+
+                if (i == 9)
+                {
+                    string l = i.ToString();
+                }
+                //Console.WriteLine(l);
+
+            }
+        }
+}
