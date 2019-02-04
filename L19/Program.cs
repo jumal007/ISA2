65a1caa26d9323c69de559311cfef0a9c7645cd5
Ilja Freiberg <ilfrei@ttu.ee>
2019-02-04 16:54:57 +0200

    L19+

--- L19/Program.cs
+++ L19/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,6 +25,61 @@ namespace L19
             b1.Author = "Robert Tabor";
             b1.Title = "Microsoft .NET XML Web Services";
             b1.ISBN = "0-000-00000-0";
+            
+            // ArrayLists are dynamically sized, 
+            // cool features sorting, remove items
+            ArrayList myArrayList = new ArrayList();
+            myArrayList.Add(car1);
+            myArrayList.Add(car2);
+            myArrayList.Add(b1);
+            myArrayList.Remove(b1);
+            foreach (Car car in myArrayList)
+            {
+                Console.WriteLine(car.Make);
+            }
+            
+
+            
+            // List<T>
+            List<Car> myList = new List<Car>();
+            myList.Add(car1);
+            myList.Add(car2);
+            //myList.Add(b1);
+            foreach (Car car  in myList)
+            {
+                Console.WriteLine(car.Model);
+            }
+            
+
+            
+            // Dictionary<TKey, TValue>
+
+            Dictionary<string, Car> myDictionary = new Dictionary<string, Car>();
+
+            myDictionary.Add(car1.VIN, car1);
+            myDictionary.Add(car2.VIN, car2);
+
+            Console.WriteLine(myDictionary["B2"].Make);
+            
+
+            //string[] names = { "Bob", "Steve", "Brian", "Chuck" };
+            
+           
+
+            Console.ReadLine();
         }
+    }
+    class Car
+    {
+        public string VIN { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
     }
 }
