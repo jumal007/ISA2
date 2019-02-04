643092e64a906e23af336d92a3f6fb087eb3b518
Ilja Freiberg <ilfrei@ttu.ee>
2019-02-04 21:56:36 +0200

    L22

---
+++ L22/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace L22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Lesson22\Example.txt");
+                Console.WriteLine(content);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem");
+                Console.WriteLine("Make sure that name of the file is named correctly: Exampl.txt");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem");
+                Console.WriteLine(@"Make sure the directory C:\Lesson22\");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("i don`t know");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
