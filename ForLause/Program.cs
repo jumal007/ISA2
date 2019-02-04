d5bee2449973cdf2f5accdff7c095095832e36cb
Ilja Freiberg <ilfrei@ttu.ee>
2019-02-04 14:01:34 +0200

    class from 14

--- ForLause/Program.cs
+++ ForLause/Program.cs
@@ -8,5 +8,36 @@ namespace ForLause
         {
             Console.WriteLine("Hello World!");
         }
+        class Car
+        {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+            public string Color { get; set; }
+
+            /*
+            public Car()
+            {
+                // You could load from a configuration file,
+                // a database, etc.
+                Make = "Nissan";
+            }
+
+            public Car(string make, string model, int year, string color)
+            {
+                Make = make;
+                Model = model;
+                Year = year;
+                Color = color;
+            }
+            */
+
+            public static void MyMethod()
+            {
+                Console.WriteLine("Called the static MyMethod");
+                Console.WriteLine(Make);
+            }
+
+        }
     }
 }
