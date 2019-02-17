7da2c19b49862c647660976b65c90b8d67722c3d
Ilja Freiberg <ilfrei@ttu.ee>
2019-02-17 20:11:05 +0200

    L11

--- W3/decisions.js
+++ W3/decisions.js
@@ -1,4 +1,4 @@
-var count = 3;
+/*var count = 3;
 if(count == 4){
     console.log('count is 4');
 } else if(count > 4) {
@@ -8,3 +8,25 @@ if(count == 4){
 }else {
     console.log('count is not 4');
 }
+
+let hero = 'batman';
+switch(hero.toLowerCase()){
+    case 'superman':
+        console.log('super strenght');
+        console.log('x-ray vision');
+        break;
+    case 'batman':
+        console.log('intelligance');
+        console.log('fighting skills');
+        break;
+    default:
+        console.log('member og JLA');
+         
+}
+*/
+
+//ternary operator
+let a = 1,  b = '1';
+let result = (a==b) ? 'equal' : 'inequal';
+console.log(result);
+console.log(a!=b) ? 'not equal' : 'not inequal';
