9f6f6ee693564aad52c46fc5d88ca582ba338a40
Ilja Freiberg <ilfrei@ttu.ee>
2019-02-18 12:49:22 +0200

    L22

---
+++ W3/natives.js
@@ -0,0 +1,10 @@
+let pattern = /xyz/;
+let value = 'This is xyz test.';
+console.log(value.replace(pattern, 'just'));
+let myString = new String('howdy');
+console.log(myString.toString());
+
+let myNumber = new Number(7);
+console.log(typeof myNumber);
+let myPrimitive = myNumber.valueOf();
+console.log(typeof myPrimitive);
